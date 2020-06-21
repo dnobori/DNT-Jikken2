3eee925df86990bc0b4de7e90af87a249aaa0a29
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2020-06-21 23:00:52 +0900

    DNT-Jikken2

---
+++ cs-process-leak-test1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace cs_process_leak_test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; ; i++)
+            {
+                if ((i % 100) == 0)
+                {
+                    Console.WriteLine(i);
+                    GC.Collect();
+                }
+
+                try
+                {
+                    ProcessStartInfo info = new ProcessStartInfo()
+                    {
+                        FileName = "/bin/uname",
+                        UseShellExecute = false,
+
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = false,
+                        RedirectStandardInput = false,
+
+                        CreateNoWindow = false,
+                        Arguments = "-a",
+                        WorkingDirectory = "/",
+                    };
+
+                    var Proc = Process.Start(info);
+
+                    Proc.WaitForExit();
+
+                    Proc.StandardOutput.ReadToEnd();
+                    Proc.StandardOutput.Close();
+                    Proc.StandardOutput.Dispose();
+
+                    Proc.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+    }
+}
