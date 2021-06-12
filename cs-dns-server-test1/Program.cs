f7743cf1a86035f00e579ce7007ffe59551fa2bb
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-06-12 18:02:03 +0900

    test

---
+++ cs-dns-server-test1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using ARSoft.Tools.Net;
+using ARSoft.Tools.Net.Dns;
+
+namespace cs_dns_server_test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DnsServer svr = new DnsServer(new IPEndPoint(IPAddress.Any, 54), 256, 0);
+
+            svr.Start();
+
+            svr.QueryReceived += Svr_QueryReceived;
+
+            Console.Write(">");
+            Console.ReadLine();
+
+            svr.Stop();
+        }
+
+        private static async Task Svr_QueryReceived(object sender, QueryReceivedEventArgs eventArgs)
+        {
+            Console.WriteLine("a");
+        }
+    }
+}
