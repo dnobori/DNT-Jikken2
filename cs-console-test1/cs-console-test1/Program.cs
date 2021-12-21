b5be2d7d118b3198f91b599f9b0ca51c3060d79a
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-12-21 12:04:23 +0900

    test

---
+++ cs-console-test1/cs-console-test1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class TestClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello.\n");
+
+        while (true)
+        {
+            Console.Write("Input>");
+
+            string? tmp = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(tmp))
+            {
+                break;
+            }
+
+            Console.WriteLine(tmp);
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("Exit.");
+    }
+}
