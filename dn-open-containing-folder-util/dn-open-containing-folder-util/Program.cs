ed6920bb07bf2489f7cb656646a84ccd1bf080aa
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2022-01-09 16:45:10 +0900

    dn-open-containing-folder-util 実装

---
+++ dn-open-containing-folder-util/dn-open-containing-folder-util/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace dn_open_containing_folder_util
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                {
+                    MessageBox.Show("Specify file path.");
+                }
+                else
+                {
+                    string filePath = args[0];
+                    string dirPath;
+
+                    if (Directory.Exists(filePath))
+                    {
+                        dirPath = filePath;
+                    }
+                    else
+                    {
+                        dirPath = Path.GetDirectoryName(filePath);
+                    }
+
+                    ProcessStartInfo ps = new ProcessStartInfo(dirPath);
+                    ps.UseShellExecute = true;
+
+                    Process.Start(ps);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
