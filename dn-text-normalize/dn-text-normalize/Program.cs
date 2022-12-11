bd4135808d5f7a1e6111f0c40f5165b179430577
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2022-12-11 11:47:46 +0900

    dn-text-normalize

---
+++ dn-text-normalize/dn-text-normalize/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace dn_text_normalize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
