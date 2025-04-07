815461962b8fd7371ff5a4a07626fc4e76d0ea7e
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2025-04-08 02:09:50 +0900

    a

---
+++ dn-find-problematic-fontfile/enum_fonts_netconsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace enum_fonts_netconsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrivateFontCollection pf = new PrivateFontCollection();
+
+            //pf.AddFontFile(@"c:\windows\fonts\ERASLGHT.TTF");
+
+            add_all(pf, @"c:\windows\fonts");
+            add_all(pf, Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Microsoft\Windows\Fonts"));
+
+            foreach (var f in pf.Families)
+            {
+                Console.WriteLine(f.Name);
+            }
+        }
+
+        static void add_all(PrivateFontCollection pf, string dir)
+        {
+            var files = Directory.EnumerateFiles(dir);
+            foreach (var file in files)
+            {
+                Debug.WriteLine(file);
+                //Console.WriteLine(file);
+                pf.AddFontFile(file);
+            }
+        }
+    }
+}
---
+++ dn-find-problematic-fontfile/enum_fonts_netconsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般的な情報は、次の方法で制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("enum_fonts_netconsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("enum_fonts_netconsole")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定します。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("ccdc19bf-4b06-4afa-a340-47f25a162a5a")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
